bd5568be8618a100522e40b0f96279abd50e6648
김상수 <pese2912@naver.com>
2016-04-20 14:25:26 +0900

    리사이클러뷰 예제

--- SampleRecyclerView/app/src/main/java/com/example/tacademy/samplerecyclerview/MainActivity.java
+++ SampleRecyclerView/app/src/main/java/com/example/tacademy/samplerecyclerview/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.tacademy.samplerecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MyAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView)findViewById(R.id.list);
+        mAdapter = new MyAdapter();
+        recyclerView.setAdapter(mAdapter);
+
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 3, LinearLayoutManager.HORIZONTAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        initData();
+    }
+
+    private void initData() {
+        for (int i = 0; i < 40 ; i++) {
+            mAdapter.add("title : " + i);
+        }
     }
 }
--- SampleRecyclerView/app/src/main/java/com/example/tacademy/samplerecyclerview/MyViewHolder.java
+++ SampleRecyclerView/app/src/main/java/com/example/tacademy/samplerecyclerview/MyViewHolder.java
@@ -8,9 +8,13 @@ import android.widget.TextView;
  * Created by Tacademy on 2016-04-20.
  */
 public class MyViewHolder extends RecyclerView.ViewHolder {
-
-
+    TextView titleView;
     public MyViewHolder(View itemView) {
         super(itemView);
+        titleView = (TextView)itemView.findViewById(R.id.text_title);
+    }
+
+    public void setTitle(String title) {
+        titleView.setText(title);
     }
 }
