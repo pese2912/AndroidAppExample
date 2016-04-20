9d13b322d52c2091de416e0d1a5de68bacc4ab0d
김상수 <pese2912@naver.com>
2016-04-20 14:07:05 +0900

    리사이클러뷰 예제

---
+++ SampleRecyclerView/app/src/main/java/com/example/tacademy/samplerecyclerview/MyViewHolder.java
@@ -0,0 +1,16 @@
+package com.example.tacademy.samplerecyclerview;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Created by Tacademy on 2016-04-20.
+ */
+public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+    public MyViewHolder(View itemView) {
+        super(itemView);
+    }
+}
