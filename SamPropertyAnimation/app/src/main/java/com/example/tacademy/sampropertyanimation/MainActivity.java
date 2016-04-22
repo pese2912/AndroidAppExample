bd80c237de78784fb85fc5eac1f2eaf409c19fe3
김상수 <pese2912@naver.com>
2016-04-22 13:50:37 +0900

    PropertyAnimation 예제

---
+++ SamPropertyAnimation/app/src/main/java/com/example/tacademy/sampropertyanimation/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.tacademy.sampropertyanimation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextSwitcher textSwitcher;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textSwitcher = (TextSwitcher)findViewById(R.id.textSwitcher);
+        Button btn = (Button)findViewById(R.id.btn_change);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView tv = (TextView)textSwitcher.getNextView();
+                tv.setText(count);
+
+                count++;
+
+            }
+        });
+
+
+    }
+    int count = 0;
+
+
+}
