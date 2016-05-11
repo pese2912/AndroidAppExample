5eb7c5ca2cf0cb2c1e94d05f86dc8c5192e48a87
pese2912 <pese2912@naver.com>
2016-05-11 10:41:39 +0900

    페이스북 연동 예제

--- SampleFacebook/app/src/main/java/com/example/tacademy/samplefacebook/MainActivity.java
+++ SampleFacebook/app/src/main/java/com/example/tacademy/samplefacebook/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.tacademy.samplefacebook;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,15 +18,28 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.sql.Array;
+import java.util.Arrays;
 import java.util.Collections;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
     LoginManager mLoginManager;
     AccessTokenTracker mTracker;
-
     Button mloginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -40,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
            public void onSuccess(LoginResult loginResult) { //로그인 성공시
+
 
             }
 
@@ -56,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
         mloginButton= (Button)findViewById(R.id.btn_login);
         mloginButton.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
 
@@ -72,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
                             //loginButton.setText("logout");
                         }
 
+
                         @Override
                         public void onCancel() {
                             Toast.makeText(MainActivity.this, "login cancel", Toast.LENGTH_SHORT).show();
@@ -86,7 +103,8 @@ public class MainActivity extends AppCompatActivity {
                     });
 
 
-                    mLoginManager.logInWithReadPermissions(MainActivity.this, null); //로그인
+                    mLoginManager.logInWithReadPermissions(MainActivity.this, Arrays.asList("email")); //이메일 퍼미션을 추가해서 로그인 획득
+
 
                 }else{ // 토큰이 있으면 로그아웃함
 
@@ -96,7 +114,69 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button btn = (Button)findViewById(R.id.btn_my_info);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(isLogin()){
+                    new MyFacebookInfoTask().execute();
+                }
+
+            }
+        });
+
+        btn = (Button)findViewById(R.id.btn_post);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
    }
+
+
+    private static final String SERVER = "https://graph.facebook.com";
+    private static final String MY_INFO = SERVER+"/v2.6/me?fields=id,name,email&access_token=%s";
+
+    class MyFacebookInfoTask extends AsyncTask<String, Integer, String> { //내정보 가져오기
+        @Override
+        protected String doInBackground(String... params) {
+            AccessToken token = AccessToken.getCurrentAccessToken();
+            try {
+
+                URL url = new URL(String.format(MY_INFO,token.getToken()));
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                int code = conn.getResponseCode();
+                if (code >= 200 && code < 300) {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String line;
+                    StringBuilder sb = new StringBuilder();
+                    while((line=br.readLine()) != null) {
+                        sb.append(line).append("\n\r");
+                    }
+                    return sb.toString();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s != null) {
+                Toast.makeText(MainActivity.this, "info : " + s , Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 
     @Override
     protected void onStart() {
@@ -140,4 +220,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
+
+
+
 }
