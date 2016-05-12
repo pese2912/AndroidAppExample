9cc0fc8c1c57d63844a0ba6c2965cfc7405333fd
pese2912 <pese2912@naver.com>
2016-05-12 12:47:28 +0900

    구글맵 연동 예제

--- SampleGoogleLocation/app/src/main/java/com/example/tacademy/samplegooglelocation/MainActivity.java
+++ SampleGoogleLocation/app/src/main/java/com/example/tacademy/samplegooglelocation/MainActivity.java
@@ -16,9 +16,17 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks,
+        OnMapReadyCallback {
 
     GoogleApiClient mClient;
     TextView messageView;
@@ -27,48 +35,51 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         messageView = (TextView)findViewById(R.id.text_message);
-
         mClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .enableAutoManage(this, this)
                 .addConnectionCallbacks(this)
                 .build();
+        SupportMapFragment fragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        fragment.getMapAsync(this);
+    }
 
+    GoogleMap mMap;
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) { // 구글서비스 접속
-
+    public void onConnected(@Nullable Bundle bundle) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         Location location = LocationServices.FusedLocationApi.getLastLocation(mClient);
         displayMessage(location);
-
         LocationRequest request = new LocationRequest();
         request.setInterval(10000);
         request.setFastestInterval(5000);
         request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
-
-        LocationServices.FusedLocationApi.requestLocationUpdates(mClient,request, mListener);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mClient, request, mListener);
     }
 
     LocationListener mListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
             displayMessage(location);
-
         }
     };
 
-    private void displayMessage(Location location){
-        if(location != null){
-            messageView.setText("lat : "+ location.getLatitude() + ", lng : " + location.getLongitude());
-
+    private void displayMessage(Location location) {
+        if (location != null) {
+            messageView.setText("lat : " + location.getLatitude() + ", lng : " + location.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 15f);
+            if (mMap != null) {
+                mMap.animateCamera(update);
+            }
         }
-
     }
 
     @Override
